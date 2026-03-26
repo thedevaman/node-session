f41bd55ba0506ad6cfe1adf9458446eac8e7b454
thedevaman <devaman2508@gmail.com>
2026-03-26 09:04:25 +0530

    mails send web/api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import session from "express-session";
+import nodemailer from "nodemailer"
 
 const app = express();
 
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'devaman2508@gmail.com',
+        pass:'bkyi fqlo cypd svzq'
+    }
+});
+
 app.set("view engine",'ejs')
-app.use(express.urlencoded({extended:true})) //used to make requested body in readable form
+app.use(express.urlencoded({extended:false})) //used to make requested body in readable form
+app.use(express.json())
 
 app.set('views', './view') //custome folder
 
@@ -29,6 +39,50 @@ app.get('/',(req,res)=>{
     res.render('home',{data})
 })
 
+app.get('/mail',(req,res)=>{
+    res.render('mail')
+})
 
+
+app.post('/submit-email',(req,res)=>{
+    console.log(req.body)
+
+    const mailOptions = {
+        from:'devaman2508@gmail.com',
+        to:'aman.vns98@gmail.com',
+        subject:req.body.subject,
+        text:req.body.message
+    }
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error)
+        {
+            req.send('something went wrong')
+        }else{
+            req.send('mail sent')
+        }
+    })
+    res.send('Email sent')
+})
+
+
+app.post('/submit-email',(req,res)=>{
+    console.log(req.body)
+
+    const mailOptions = {
+        from:'devaman2508@gmail.com',
+        to:'aman.vns98@gmail.com',
+        subject:req.body.subject,
+        text:req.body.message
+    }
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error)
+        {
+            req.send('something went wrong')
+        }else{
+            req.send('mail sent')
+        }
+    })
+    res.send('Email sent')
+})
 
 app.listen(3200)
